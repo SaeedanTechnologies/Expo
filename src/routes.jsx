fb8985688056c57590d64a7a7182b9a8b5c92eab
Muhammad Moaz <moaz@saeedantechpvt.com>
2024-06-15 14:32:43 +0500

    xyz

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,8 @@
 import { useRoutes } from "react-router";
 import Landing from "./page/landing/Landing";
+import AdminHome from "./AuthSide/AdminSide1";
+import Welcome from "./AuthSide/AdminSide1/Welcome"
+import AddEvent from "./AuthSide/AdminSide1/AddEvent";
 
 export default function Router() {
     let element = useRoutes([
@@ -7,6 +10,22 @@ export default function Router() {
             path: '/',
             element: <Landing />,
         },
+        {
+            path: '/admin',
+            element: <AdminHome />,
+            children: [
+                {
+                    path: 'welcome',
+                    element: <Welcome />,
+                },
+                {
+                    path: 'add-event',
+                    element: <AddEvent />,
+                },
+
+            ]
+        },
+
 
 
     ])
