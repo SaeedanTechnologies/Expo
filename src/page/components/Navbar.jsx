bd8280621e2e1786395799a1556d271255dd2a26
Ziaurrehm <irfankhan@saeedantechpvt.com>
2024-06-20 10:57:20 +0500

    resolve issues

--- src/page/components/Navbar.jsx
+++ src/page/components/Navbar.jsx
@@ -1,15 +1,5 @@
 import React, { useState } from "react";
-import {
-  Avatar,
-  Box,
-  Button,
-  Drawer,
-  FormControl,
-  MenuItem,
-  Select,
-  Typography,
-  IconButton,
-} from "@mui/material";
+import { Box, Button, Drawer, FormControl, MenuItem, Select, Typography, IconButton, Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 
@@ -25,129 +15,62 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <Box
-        sx={{
-          backgroundColor: "white",
-          padding: "0rem 5%",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          position: "static",
-          top: 0,
-          zIndex: 10000000,
-          boxShadow: "1px 1px 1px #ededed",
-        }}
-      >
-        <Box>
-          <Link to="/">
-            <img
-              src="/mainLogo.png"
-              alt="Logo"
-              style={{ height: "auto", width: "100%" }}
-            />
-          </Link>
-        </Box>
-
-        {/* Desktop view */}
-        <Box
-          sx={{
-            display: { xs: "none", sm: "flex" }, // Hide on extra small screens (mobile)
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Box>
-            <Button variant="contained" sx={{ textTransform: "none" }}>
-              Upgrade your pricing plan
-            </Button>
-          </Box>
-
-          <FormControl sx={{ padding: 0 }}>
-            <Select
-              sx={{
-                outline: "none",
-                "&:focus": {
-                  outline: "none",
-                },
-                "& .MuiOutlinedInput-notchedOutline": {
-                  border: "none", // Remove the outline border
-                },
-              }}
-              displayEmpty
-              inputProps={{ "aria-label": "Select user" }}
-              style={{ minWidth: "120px", padding: 0 }}
-              renderValue={(selected) => (
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <Avatar />
-                  <Typography sx={{ fontSize: "1rem" }}> user</Typography>
-                </Box>
-              )}
-            >
-              <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Profile</MenuItem>
-              <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Judges</MenuItem>
-              <MenuItem sx={{ fontSize: "0.8rem" }}>Logout</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-
-        {/* Mobile view */}
-        <Box sx={{ display: { xs: "flex", sm: "none" } }}>
-          <IconButton onClick={handleDrawerOpen} sx={{ padding: "10px" }}>
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            anchor="right"
-            open={drawerOpen}
-            onClose={handleDrawerClose}
-            sx={{ zIndex: 10000000 }}
-          >
-            <Box sx={{ width: 250, padding: "20px" }}>
-
-
-
-            <Box
-
-        >
-          <Box>
-            <Button variant="contained" sx={{ textTransform: "none" }}>
-              Upgrade your pricing plan
-            </Button>
-          </Box>
-
-          <FormControl sx={{ padding: 0 }}>
-            <Select
-              sx={{
-                outline: "none",
-                "&:focus": {
-                  outline: "none",
-                },
-                "& .MuiOutlinedInput-notchedOutline": {
-                  border: "none",
-                },
-              }}
-              displayEmpty
-              inputProps={{ "aria-label": "Select user" }}
-              style={{ minWidth: "120px", padding: 0 }}
-              renderValue={(selected) => (
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <Avatar />
-                  <Typography sx={{ fontSize: "1rem" }}> user</Typography>
-                </Box>
-              )}
-            >
-              <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Profile</MenuItem>
-              <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Judges</MenuItem>
-              <MenuItem sx={{ fontSize: "0.8rem" }}>Logout</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-
-            </Box>
-          </Drawer>
-        </Box>
+    <Box sx={{ backgroundColor: "white", padding: "0rem 5%", display: "flex", justifyContent: "space-between", alignItems: "center", position: "static", top: 0, zIndex: 10000000, boxShadow: "1px 1px 1px #ededed" }}>
+      <Box>
+        <Link to="/">
+          <img src="/mainLogo.png" alt="Logo" style={{ height: "auto", width: "100%" }} />
+        </Link>
       </Box>
-    </>
+      <Box sx={{ display: { xs: "none", sm: "flex" }, justifyContent: "center", alignItems: "center" }}>
+        <Button variant="contained" sx={{ textTransform: "none" }}>Upgrade your pricing plan</Button>
+        <FormControl sx={{ padding: 0 }}>
+          <Select
+            sx={{ outline: "none", "&:focus": { outline: "none" }, "& .MuiOutlinedInput-notchedOutline": { border: "none" } }}
+            displayEmpty
+            inputProps={{ "aria-label": "Select user" }}
+            style={{ minWidth: "120px", padding: 0 }}
+            renderValue={() => (
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Avatar />
+                <Typography sx={{ fontSize: "1rem" }}> user</Typography>
+              </Box>
+            )}
+          >
+            <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Profile</MenuItem>
+            <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Judges</MenuItem>
+            <MenuItem sx={{ fontSize: "0.8rem" }}>Logout</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <Box sx={{ display: { xs: "flex", sm: "none" } }}>
+        <IconButton onClick={handleDrawerOpen} sx={{ padding: "10px" }}>
+          <MenuIcon />
+        </IconButton>
+        <Drawer anchor="right" open={drawerOpen} onClose={handleDrawerClose} sx={{ zIndex: 10000000 }}>
+          <Box sx={{ width: 250, padding: "20px" }}>
+            <Button variant="contained" sx={{ textTransform: "none" }}>Upgrade your pricing plan</Button>
+            <FormControl sx={{ padding: 0 }}>
+              <Select
+                sx={{ outline: "none", "&:focus": { outline: "none" }, "& .MuiOutlinedInput-notchedOutline": { border: "none" } }}
+                displayEmpty
+                inputProps={{ "aria-label": "Select user" }}
+                style={{ minWidth: "120px", padding: 0 }}
+                renderValue={() => (
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Avatar />
+                    <Typography sx={{ fontSize: "1rem" }}> user</Typography>
+                  </Box>
+                )}
+              >
+                <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Profile</MenuItem>
+                <MenuItem sx={{ fontSize: "0.8rem" }}>Manage Judges</MenuItem>
+                <MenuItem sx={{ fontSize: "0.8rem" }}>Logout</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Drawer>
+      </Box>
+    </Box>
   );
 };
 
