ee64072915158f1a639bcdeecaa886f0b3c4bff4
Ziaurrehm <irfankhan@saeedantechpvt.com>
2024-06-25 20:47:00 +0500

    add verifycaations api and generate otp

--- src/AuthSide/AdminSide1/AddEvent.jsx
+++ src/AuthSide/AdminSide1/AddEvent.jsx
@@ -1,17 +1,155 @@
-import React, { useState } from 'react';
-import MyTextField from '../../page/components/MyTextField';
-import { Box, Typography, Snackbar, CircularProgress } from '@mui/material';
-import MyButton from '../../page/components/MyButton';
-import { useNavigate } from 'react-router';
-import axios from 'axios';
+// import React, { useState } from "react";
+// import MyTextField from "../../page/components/MyTextField";
+// import { Box, Typography, Snackbar, CircularProgress } from "@mui/material";
+// import MyButton from "../../page/components/MyButton";
+// import { useNavigate } from "react-router";
+// import axios from "axios";
+// import { useDispatch } from "react-redux";
+// const AddEvent = () => {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const [eventName, setEventName] = useState("");
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState("");
+//   const [snackbarOpen, setSnackbarOpen] = useState(false);
+//   const [snackbarMessage, setSnackbarMessage] = useState("");
+
+//   const handleEventNameChange = (event) => {
+//     setEventName(event.target.value);
+//   };
+
+//   const handleSubmit = async () => {
+//     if (!eventName.trim()) {
+//       setError("Event Name is required");
+//       return;
+//     }
+
+//     const token = localStorage.getItem("token");
+
+//     if (!token) {
+//       setError("No authentication token found. Please log in.");
+//       setSnackbarMessage("No authentication token found. Please log in.");
+//       setSnackbarOpen(true);
+//       return;
+//     }
+
+//     try {
+//       setLoading(true);
+//       const response = await axios.post(
+//         "https://expoproject.saeedantechpvt.com/api/admin/expo",
+//         { name: eventName },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         }
+//       );
+//       localStorage.setItem("expo_id", response.data.payload.id);
+//       console.log("expo_id", response.data.payload.id);
+//       setSnackbarMessage("Event added successfully!");
+//       setSnackbarOpen(true);
+//       navigate("/admin/add-content");
+//     } catch (error) {
+//       console.error("Error:", error);
+//       setSnackbarMessage("An error occurred. Please try again later.");
+//       setSnackbarOpen(true);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   const handleSnackbarClose = (event, reason) => {
+//     if (reason === "clickaway") {
+//       return;
+//     }
+//     setSnackbarOpen(false);
+//   };
+
+//   return (
+//     <Box
+//       sx={{
+//         display: "flex",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         minHeight: "80vh",
+//         padding: "1rem 10%",
+//       }}
+//     >
+//       <Box
+//         sx={{
+//           display: "flex",
+//           flexDirection: "column",
+//           gap: "20px",
+//           width: "400px",
+//           margin: "0 auto",
+//         }}
+//       >
+//         <Typography
+//           sx={{
+//             fontSize: "36px",
+//             fontWeight: 700,
+//             textAlign: "center",
+//           }}
+//         >
+//           Add Event
+//         </Typography>
+//         <Typography
+//           sx={{
+//             color: "#949494",
+//             fontSize: "16px",
+//             fontWeight: 300,
+//             textAlign: "center",
+//           }}
+//         >
+//           Lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet
+//           consectetur lorem ipsum dolor sit amet.
+//         </Typography>
+//         <Box>
+//           <MyTextField
+//             label="Event Name"
+//             placeholder="Please Write Convention Name"
+//             value={eventName}
+//             onChange={handleEventNameChange}
+//             error={!!error}
+//             helperText={error}
+//           />
+//         </Box>
+//         <MyButton
+//           onClick={handleSubmit}
+//           text={loading ? <CircularProgress size={24} /> : "Next"}
+//           disabled={loading}
+//         />
+//       </Box>
+     
+//       <Snackbar
+//         open={snackbarOpen}
+//         autoHideDuration={6000}
+//         onClose={handleSnackbarClose}
+//         message={snackbarMessage}
+//       />
+//     </Box>
+//   );
+// };
+
+// export default AddEvent;
+
+
+import React, { useState } from "react";
+import MyTextField from "../../page/components/MyTextField";
+import { Box, Typography, Snackbar, CircularProgress } from "@mui/material";
+import MyButton from "../../page/components/MyButton";
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { addEvent } from "../../store/actions/adminActions";
 
 const AddEvent = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [eventName, setEventName] = useState('');
+  const [eventName, setEventName] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarMessage, setSnackbarMessage] = useState("");
 
   const handleEventNameChange = (event) => {
     setEventName(event.target.value);
@@ -19,48 +157,38 @@ const AddEvent = () => {
 
   const handleSubmit = async () => {
     if (!eventName.trim()) {
-      setError('Event Name is required');
+      setError("Event Name is required");
       return;
     }
 
-    const token = localStorage.getItem('token'); // Retrieve the token from local storage
+    const token = localStorage.getItem("token");
 
     if (!token) {
-      setError('No authentication token found. Please log in.');
-      setSnackbarMessage('No authentication token found. Please log in.');
+      setError("No authentication token found. Please log in.");
+      setSnackbarMessage("No authentication token found. Please log in.");
       setSnackbarOpen(true);
       return;
     }
 
     try {
       setLoading(true);
-      const response = await axios.post(
-        'https://expoproject.saeedantechpvt.com/api/admin/expo',
-        { name: eventName },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`, 
-          },
-        }
-      );
+      const response = await dispatch(addEvent({ name: eventName }));
       localStorage.setItem("expo_id", response.data.payload.id);
-      console.log('expo_id', response.data.payload.id);
-      setSnackbarMessage('Event added successfully!');
+      console.log("expo_id", response.data.payload.id);
+      setSnackbarMessage("Event added successfully!");
       setSnackbarOpen(true);
-      navigate('/admin/add-content');
+      navigate("/admin/add-content");
     } catch (error) {
-      console.error('Error:', error);
-      setSnackbarMessage('An error occurred. Please try again later.');
+      console.error("Error:", error);
+      setSnackbarMessage("An error occurred. Please try again later.");
       setSnackbarOpen(true);
     } finally {
       setLoading(false);
     }
   };
 
-
-
   const handleSnackbarClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setSnackbarOpen(false);
@@ -69,40 +197,41 @@ const AddEvent = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '80vh',
-        padding: '1rem 10%',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "80vh",
+        padding: "1rem 10%",
       }}
     >
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '20px',
-          width: '400px',
-          margin: '0 auto',
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          width: "400px",
+          margin: "0 auto",
         }}
       >
         <Typography
           sx={{
-            fontSize: '36px',
+            fontSize: "36px",
             fontWeight: 700,
-            textAlign: 'center',
+            textAlign: "center",
           }}
         >
           Add Event
         </Typography>
         <Typography
           sx={{
-            color: '#949494',
-            fontSize: '16px',
+            color: "#949494",
+            fontSize: "16px",
             fontWeight: 300,
-            textAlign: 'center',
+            textAlign: "center",
           }}
         >
-          Lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet.
+          Lorem ipsum dolor sit amet consectetur lorem ipsum dolor sit amet
+          consectetur lorem ipsum dolor sit amet.
         </Typography>
         <Box>
           <MyTextField
@@ -114,9 +243,13 @@ const AddEvent = () => {
             helperText={error}
           />
         </Box>
-        <MyButton onClick={handleSubmit} text={loading ? <CircularProgress size={24} /> : 'Next'} disabled={loading} />
+        <MyButton
+          onClick={handleSubmit}
+          text={loading ? <CircularProgress size={24} /> : "Next"}
+          disabled={loading}
+        />
       </Box>
-      {/* Snackbar for notifications */}
+     
       <Snackbar
         open={snackbarOpen}
         autoHideDuration={6000}
@@ -128,3 +261,5 @@ const AddEvent = () => {
 };
 
 export default AddEvent;
+
+
--- src/store/actions/adminActions.jsx
+++ src/store/actions/adminActions.jsx
@@ -1,11 +1,24 @@
 import api from "../../utils/Api";
 
-export const adminRegister = (formValues) => async (dispatch) => {
+export const addEvent = (formValues) => async (dispatch) => {
   try {
-    const res = await api.post("admin/register", formValues);
-
+    const res = await api.post("admin/expo", formValues);
+    dispatch({ type: "ADD_EVENT_SUCCESS", payload: res.data });
     return res;
   } catch (err) {
+    dispatch({ type: "ADD_EVENT_FAILURE", error: err });
     throw err;
   }
 };
+
+export const addContest = (formValues) => async (dispatch) => {
+    try {
+      const res = await api.post("admin/expo", formValues);
+      dispatch({ type: "ADD_EVENT_SUCCESS", payload: res.data });
+      return res;
+    } catch (err) {
+      dispatch({ type: "ADD_EVENT_FAILURE", error: err });
+      throw err;
+    }
+  };
+  
