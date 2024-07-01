0808c9733b9123dd8edf58b10b4af0bc69c749b6
Usama Jawad <usama@saeedantechpvt.com>
2024-07-01 18:58:17 +0500

    usama updated code

--- src/store/actions/contestStartActions.jsx
+++ src/store/actions/contestStartActions.jsx
@@ -3,8 +3,6 @@ import api from "../../utils/Api";
 export const getStartContest = (contestId) => async (dispatch) => {
   try {
     const res = await api.get(`admin/contest-start/${contestId}`);
-console.log(res, 'response from contest start api')
-
     return res;
   } catch (err) {
     throw err;
@@ -17,8 +15,6 @@ console.log(res, 'response from contest start api')
 export const setNextParticipant = (contestId, participant_id) => async (dispatch) => {
     try {
       const res = await api.post(`admin/judge-participant/${contestId}`, {participant_id});
-  console.log(res, 'response from dge set participant')
-
       return res;
     } catch (err) {
       throw err;
@@ -30,8 +26,6 @@ export const setNextParticipant = (contestId, participant_id) => async (dispatch
   export const getAllRecords = (contestId) => async (dispatch) => {
     try {
       const res = await api.get(`admin/contest-result/${contestId}`);
-  
-
       return res;
     } catch (err) {
       throw err;
