e9daa97ceeb8e9eb883192cf29d56c52b839db5f
Ziaurrehm <irfankhan@saeedantechpvt.com>
2024-07-10 16:59:55 +0500

    changes event changes updated

--- src/store/actions/adminActions.jsx
+++ src/store/actions/adminActions.jsx
@@ -11,6 +11,33 @@ export const addEvent = (formValues) => async (dispatch) => {
   }
 };
 
+export const editEvent = (id, formValues) => async (dispatch) => {
+  try {
+    // Assuming 'api' is your Axios instance or API utility
+    const res = await api.put(`admin/expo/${id}`, {
+      ...formValues,
+    
+    });
+    dispatch({ type: "ADD_EVENT_SUCCESS", payload: res.data });
+    return res;
+  } catch (err) {
+    dispatch({ type: "ADD_EVENT_FAILURE", error: err });
+    throw err;
+  }
+};
+
+
+export const getEvent = (id) => async (dispatch) => {
+  try {
+    const res = await api.get(`admin/expo/${id}`);
+    dispatch({ type: "ADD_EVENT_SUCCESS", payload: res.data });
+    return res;
+  } catch (err) {
+    dispatch({ type: "ADD_EVENT_FAILURE", error: err });
+    throw err;
+  }
+};
+
 export const addContest = (formValues) => async (dispatch) => {
     try {
       const res = await api.post("admin/expo", formValues);
--- src/store/reducers/index.jsx
+++ src/store/reducers/index.jsx
@@ -1,8 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import adminReducer from './adminReducer'
+import eventReucer from "./eventSlice";
 const rootReducer = combineReducers({
     admin: adminReducer,
-
+    events: eventReucer,
     
 })
 
